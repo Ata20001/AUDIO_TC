55d0329ea912f7315f9d97c16ba7f34790a05166
Ata Haider <ata.haider@example.com>
2026-06-12 23:16:42 +0530

    jwt/login

--- controller/authController.js
+++ controller/authController.js
@@ -1,39 +1,107 @@
-const User = require('../model/user');
-const bcrypt = require('bcrypt');
+const User = require("../model/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
-    try {
+  try {
+    const { name, email, password } = req.body;
 
-        const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-        const existingUser = await User.findOne({ email });
-
-        if (existingUser) {
-            return res.status(400).json({
-                message: "User already exists"
-            });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = await User.create({
-            name,
-            email,
-            password: hashedPassword
-        });
-
-        res.status(201).json({
-            message: "User registered successfully",
-            user
-        });
-
-    } catch (err) {
-        console.log(err);
-
-        res.status(500).json({
-            message: "Server Error"
-        });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const userData = {
+      id: user._id,
+      email: user.email,
+    };
+
+    const token = jwt.sign(
+      userData,
+      process.env.jwt_secretKey,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(201).json({
+      message: "User registered successfully",
+      token,
+    });
+
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
 };
 
-module.exports = register;
+
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(404).json({
+        message: "Please register first",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Please provide the correct password",
+      });
+    }
+
+    const userData = {
+      id: existingUser._id,
+      email: existingUser.email,
+    };
+
+    const token = jwt.sign(
+      userData,
+      process.env.jwt_secretKey,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+  
+
+module.exports = {register  , login };
+
